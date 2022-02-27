3cb656cb12cab4850a5e14ff64ba172a25f8b8ea
Prince Kumar <prince.kumar267@gmail.com>
2022-02-27 14:50:09 +0530

    new files

---
+++ src/App/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import SideBar from "../components/sideBar"
+import Main from "../components/main"
+const App = ()=>{
+    return(
+        <div>
+            <SideBar/>
+            <Main/>
+        </div>
+    )
+}
+
+export default App;
